176b8a8941e9138b48f6c563c05e4d3085022e85
jrim <comfortindex@naver.com>
2022-12-21 19:27:35 +0900

    221219 class

---
+++ class_221219.m
@@ -0,0 +1,40 @@
+clc;
+clear;
+close all;
+
+% class time?
+% extra team practice:
+    % 1) ocean pCO2 matrix data comparison/correlation
+    % 2) atmospheric data (PM, AOD etc...) comparison/correlation
+    % 3) dust and CO2(or chlorophyll) data comparison/correlation
+
+% toolbox: pre-installed useful functions (some are GUI available)
+    % path must be designated accurately (home > set path)
+    % and function must exist in "the path"
+
+% matrix calculation
+    % element-wise
+    % flipping matrix: flip, flipud, fliplr
+    % transpose, reshape
+% size of matrix: size and length
+
+% colon and semi-colon
+% end: last element
+% seperately saving variables on workspace
+% data format
+% 'print' is for saving file into specific file format
+
+% string functions: C-based functions and more (ex. strcmpi)
+
+% observing devices have unique error code (errno)
+
+% other calculations
+    % cell, floor, round, fix
+    % diff
+
+% function handle
+
+
+
+
+
